4095f2a480a8fcaa523aa6704695a809e8fccf29
Angelogama21 <ang21gm33@gmail,com>
2024-02-23 21:20:26 -0600

    Implementación de imagenes

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const HomePage = () => {
   return (
-    <main></main>
-  )
-}
+    <main className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <img src="/barou.jpg" alt="Descripción de la imagen" className="w-80 h-80 object-cover rounded-full mx-auto" />
+        <p className="mt-4">Shouei Barou, delantero del proyecto Blue Look</p>
+      </div>
+    </main>
+  );
+};
 
-export default HomePage
+export default HomePage;
